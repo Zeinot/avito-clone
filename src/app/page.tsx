63d51f6ba51f6f1ad218946fc106e0224506b0dc
Zeinot <zeinotgaming@gmail.com>
2024-06-15 19:51:10 +0100

    s

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
-export default function page() {
-  return (
-    <div>page</div>
-  )
+import prisma from "@/db";
+export const dynamic = "force-dynamic";
+
+export default async function page() {
+  const test = await prisma.test.findMany();
+  console.log(test);
+
+  return <div>page</div>;
 }
