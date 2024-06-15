1d009edf4aa0315a9bfdd3a3b9c20b446697501c
Zeinot <zeinotgaming@gmail.com>
2024-06-15 21:38:16 +0100

    s

--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,9 @@
 import NextAuth from "next-auth";
 import prisma from "./db";
+import Keycloak from "next-auth/providers/keycloak";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [],
+  providers: [Keycloak],
 });
