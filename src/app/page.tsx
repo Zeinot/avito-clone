36279a6c3440d8c8cf03567578adb4c68ade598a
Zeinot <zeinotgaming@gmail.com>
2024-06-15 20:05:30 +0100

    s

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
 import prisma from "@/db";
 export const dynamic = "force-dynamic";
+import { z } from "zod";
 
+// creating a schema for strings
+const mySchema = z.string();
+
+// parsing
+console.log(mySchema.parse("tuna"));
 export default async function page() {
   return <></>;
 }
