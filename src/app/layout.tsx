600a0ddd5097c5ea709f3169372f847566c6233c
Zeinot <zeinotgaming@gmail.com>
2024-06-15 23:14:24 +0100

    S

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/layouts/Navbar";
+import Footer from "@/components/layouts/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +19,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        <div className="mx-auto max-w-2xl px-4 py-8 sm:px-6 sm:py-12 lg:max-w-7xl lg:px-8">
+          {children}
+        </div>
+        <Footer />
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import { SignIn } from "@/components/auth/server/signin-button";
+import Categories from "@/components/Categories/Categories";
+import Listings from "@/components/Listings/Listings";
 import prisma from "@/db";
 export const dynamic = "force-dynamic";
 import { z } from "zod";
@@ -12,7 +14,9 @@ import { z } from "zod";
 export default async function page() {
   return (
     <>
-      <SignIn />
+      {/* <SignIn /> */}
+      <Categories />
+      <Listings />
     </>
   );
 }
